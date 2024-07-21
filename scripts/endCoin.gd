b360c765b54499cf74ac88fd70f82fc7740e944e
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-07-21 16:08:12 -0600

    More levels

--- scripts/endCoin.gd
+++ scripts/endCoin.gd
@@ -14,3 +14,7 @@ func _on_body_entered(body):
 	if body is CharacterBody2D:
 		match currentScene:
 			"tutorial": get_tree().change_scene_to_file("res://levels/level1.tscn")
+			"level1": get_tree().change_scene_to_file("res://levels/level2.tscn")
+			"level2": get_tree().change_scene_to_file("res://levels/level3.tscn")
+			"level3": get_tree().change_scene_to_file("res://levels/level4.tscn")
+			"level4": get_tree().change_scene_to_file("res://levels/level5.tscn")
